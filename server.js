7c24a1a0b2c683b8d0b000a4b04331ab0e22f33c
Ajita MISHRA <ajita00054@gmail.com>
2021-11-27 12:24:38 +0530

    server changes

--- server.js
+++ server.js
@@ -1,29 +1,38 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const path = require("path");
-const mongoose = require("mongoose");
+const express=require('express');
+const bodyParser=require('body-parser');
+const path=require('path');
+const crypto=require('crypto');
+const mongoose=require('mongoose');
+const multer=require('multer');
+const GridFsStorage=require('multer-gridfs-storage');
+const Grid=require('gridfs-stream');
+const methodoverride=require('method-override');
 const passport = require("passport");
 const users = require("./routes/api/users");
 const subjects = require("./routes/api/subjects");
-const cors = require("cors");
+const cors = require('cors')
 require("dotenv").config();
 
-const app = express();
-app.use(cors());
+
+const app=express();
+app.use(cors())
 app.use(
-  bodyParser.urlencoded({
-    extended: false,
-  })
-);
-app.use(bodyParser.json());
+    bodyParser.urlencoded({
+      extended: false
+    })
+  );
+  app.use(bodyParser.json());
 
 // DB Config
 const db = require("./config/keys").mongoURI;
 // Connect to MongoDB
 mongoose
-  .connect(db, { useNewUrlParser: true })
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
   .then(() => console.log("MongoDB successfully connected"))
-  .catch((err) => console.log(err));
+  .catch(err => console.log(err));
 
 // Passport middleware
 app.use(passport.initialize());
@@ -31,9 +40,8 @@ app.use(passport.initialize());
 require("./config/passport")(passport);
 // Routes
 
+
 app.use("/api/users", users);
 app.use("/api/subjects", subjects);
 
-app.listen(process.env.PORT || 5000, () =>
-  console.log(`Server up and running on port ${port} !`)
-);
+app.listen( process.env.PORT || 5000, () => console.log(`Server up and running on port ${port} !`));
